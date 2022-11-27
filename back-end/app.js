550bc10c4b9073d3cf7a23a8f559939b245d4f66
RicardoMendess <ricardomendesprofissional@gmail.com>
2022-11-27 11:52:06 -0300

    fixed: project stream init

---
+++ back-end/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get("/videos", (req, res) => {
+
+})
+
+app.post("/EnviarVideo", (req, res) => {
+
+})
+
+app.put("/EnviarVideo", (req, res) => {
+    
+})
+
+app.delete("/EnviarVideo", (req, res) => {
+    
+})
